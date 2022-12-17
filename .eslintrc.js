5617c93366e2f984aeb76fedd0f7332eac520253
Takeshi Hashimoto <htakeshi0614@gmail.com>
2022-12-17 15:30:42 -0800

    solved null error

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,6 @@ module.exports = {
     "extends": [
         "plugin:react/recommended",
         "standard-with-typescript",
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'prettier',
     ],
     "overrides": [
