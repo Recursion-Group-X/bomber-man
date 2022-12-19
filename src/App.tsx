834feffd789fdf72ff248709588ab1133cd29202
Takeshi Hashimoto <htakeshi0614@gmail.com>
2022-12-18 18:09:36 -0800

    ディレクトリ構造とルーティングの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { VFC } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Game from './pages/Game';
+import Home from './pages/Home';
+import Result from './pages/Result';
+import StageList from './pages/StageList';
 
-const App: VFC = () => {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="">
+        My App
+      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='stage-list/' element={<StageList />} />
+          <Route path='game/' element={<Game />} />
+          <Route path='result/' element={<Result />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+    
   );
 }
 
