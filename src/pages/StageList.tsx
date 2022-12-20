3f2f4eb4f08590ef17f1c7433a586823925dedbe
Takeshi Hashimoto <htakeshi0614@gmail.com>
2022-12-19 20:15:09 -0800

    各ページにnavigate

--- src/pages/StageList.tsx
+++ src/pages/StageList.tsx
@@ -1,11 +1,18 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const StageList: React.FC = () => {
+  const navigate = useNavigate()
   const [stages, setStages] = useState<string[]>(['stage1', 'stage2', 'stage3', 'stage4', 'stage5', 'stage6'])
+  const navigateHome = (): void => {
+    navigate('/')
+  }
+
+
   return (
     <div className='h-screen text-xl'>
       <div className='h-20 bg-slate-600 flex items-center justify-center'>
-        <p className='text-center text-lg text-white'>STAGE SELECt</p>
+        <p className='text-center text-2xl text-white'>STAGE SELECt</p>
       </div>
       <div className='w-2/3 mx-auto flex flex-wrap'>
         {stages.map( stage =>
@@ -16,7 +23,7 @@ const StageList: React.FC = () => {
         )}
       </div>
       <div className='absolute right-36'>
-          <button className='p-2 w-20 border border-4'>OK</button>
+          <button className='p-2 w-20 border border-4' onClick={navigateHome}>OK</button>
       </div>
     </div>
   )
