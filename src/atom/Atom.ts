39b52cc956680dca1d0fa91344f179e68b835ab6
Takeshi Hashimoto <htakeshi0614@gmail.com>
2022-12-21 22:08:18 -0800

    ブロックの位置に応じてプレイヤーの移動を制限

--- src/atom/Atom.ts
+++ src/atom/Atom.ts
@@ -1,4 +1,5 @@
 import { atom, PrimitiveAtom } from "jotai"
+import { Player } from "../models/Player"
 
 
 export const canvasRefAtom: any = atom(null)
@@ -6,7 +7,7 @@ export const currentStageAtom: PrimitiveAtom<number[][]> = atom(
     [
         [0,0,1,0,0,1,1,0,1,0,0,0,0,0,0],
         [0,2,1,2,0,2,0,2,0,2,0,2,0,2,0],
-        [0,0,1,1,0,1,0,0,0,0,0,0,0,0,0],
+        [0,0,0,1,0,1,0,0,0,0,0,0,0,0,0],
         [1,2,0,2,0,2,0,2,0,2,0,2,0,2,0],
         [0,0,0,0,0,0,1,1,0,1,0,0,0,0,0],
         [1,2,0,2,0,2,0,2,0,2,1,2,1,2,0],
@@ -21,3 +22,5 @@ export const currentStageAtom: PrimitiveAtom<number[][]> = atom(
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
   ]
 )
+
+export const playerAtom = atom<Player>(new Player('player1'))
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -3,40 +3,36 @@ import React, { useEffect, useRef, useState } from 'react'
 import grassImg from '../assets/grass.png'
 import stoneImg from '../assets/stone.png'
 import wallImg from '../assets/wall.png'
-import { currentStageAtom } from '../atom/Atom'
-import { Player } from '../models/Player'
+import { currentStageAtom, playerAtom } from '../atom/Atom'
 
 const Game: React.FC = () => {
   const [currentStage] = useAtom(currentStageAtom)
   const gameCanvasRef = useRef<HTMLCanvasElement>(null)
   const [canvasContext, setCavnasContext] = useState<CanvasRenderingContext2D | null | undefined>(null)
-  const [player, setPlayer] = useState<Player | null>(null)
-
+  const [player] = useAtom(playerAtom)
+ 
   useEffect(() => {
     if(gameCanvasRef != null){
       const a = gameCanvasRef.current?.getContext('2d')
       setCavnasContext(a)
-      drawPlayer()
+      player.draw(canvasContext)
     }
+    
     removeEventListener('keydown', changePlayerDirection)
     removeEventListener('keyup', stopPlayer)
     addEventListener('keydown', changePlayerDirection)
     addEventListener('keyup', stopPlayer)
-    // 0.05秒ごとに状態を更新する
+
+    // 0.05秒ごとに状態を更新する // このインターバルでエネミーも移動させる
     const intervalId = setInterval(() => {
       // インターバルで方向に基づいて移動する
-      player?.move(canvasContext)
+      player?.move(canvasContext, currentStage)
     }, 50);
     return () => {
       clearInterval(intervalId);
     };
   },[canvasContext])
 
-  const drawPlayer = (): void => {
-    const p = new Player('tck', 0, 0, 40, 40)
-    setPlayer(p)
-    p.draw(canvasContext)
-  }
 
   // Playerの移動方向を変える
   const changePlayerDirection = (e: any): void => {
@@ -45,7 +41,6 @@ const Game: React.FC = () => {
 
   // プレイヤーの移動を止める
   const stopPlayer = (e: any): void => {
-    console.log('stop')
     player?.stopPlayer(e)
   }
 
@@ -56,19 +51,13 @@ const Game: React.FC = () => {
           <p className='ml-10 text-xl text-white'>00:00</p>
         </div>
         <div className='w-1/3 mx-auto flex justify-around'>
-          <div>
-            Item1: 
-          </div>
-          <div>
-            Item2:
-          </div>
-          <div>
-            Item3:
-          </div>
+          <div>Item1: </div>
+          <div>Item2:</div>
+          <div>Item3:</div>
         </div>
       </div>
 
-      <div className=' mx-auto bg-white mt-6 flex' style={{height: '500px', width: '500px'}}>
+      <div className=' mx-auto bg-white mt-12 flex' style={{height: '500px', width: '500px'}}>
         <table className='h-full w-full'>
           {currentStage.map(row => 
             <tr className={`h-1/${currentStage.length} w-full`} key={row[0]}>
@@ -78,7 +67,6 @@ const Game: React.FC = () => {
                  box === 1 ? <td className={`w-1/${row.length}`} key={box} style={{backgroundImage:`url(${stoneImg})`}}></td>:
                              <td className={`w-1/${row.length}`} key={box} style={{backgroundImage:`url(${wallImg})`}}></td>
                 }
-                
                 </>
               )}
             </tr>
