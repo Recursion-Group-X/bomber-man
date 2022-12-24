ed240afe0c6ecc3f578b47a3a9c92ff5bfbab607
Takeshi Hashimoto <htakeshi0614@gmail.com>
2022-12-23 18:15:15 -0800

    fixed bugs

--- src/models/Stage.tsx
+++ src/models/Stage.tsx
@@ -4,14 +4,12 @@ export class Stage {
     private readonly _x: number;
     private readonly _y: number;
     private readonly _numOfBreakableBlocks: number;
-    private readonly _notPutArea: number[][];
     private readonly _board: number[][];
     
     constructor(config: Config) {
         this._x = config.x;
         this._y = config.y;
         this._numOfBreakableBlocks = config.breakableBlocks;
-        this._notPutArea = config.notPutArea;
         this._board = this.createInitialStage()
     }
   
@@ -37,11 +35,11 @@ export class Stage {
     setBreakableBlock(): void {
         let currNumOfBlocks = 0;
         while(currNumOfBlocks < this._numOfBreakableBlocks){
-            const x = Math.floor(Math.random() * this._x);
-            const y = Math.floor(Math.random() * this._y);
-            if((this._board[x][y]) === 0 && !(this._notPutArea.some(position => position.includes(x,y)))){
-            this._board[x][y] = 1;
-            currNumOfBlocks += 1;
+            const x = Math.floor(Math.random() * (this._x - 1)) + 1;
+            const y = Math.floor(Math.random() * (this._y - 1)) + 1;
+            if((this._board[x][y]) === 0 && !(x <= 2 && y <= 2)){
+                this._board[x][y] = 1;
+                currNumOfBlocks += 1;
             }
         }
     }
