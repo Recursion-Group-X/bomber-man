6b01ee304e3648c706729c7653797155ea394e0b
Koki Arakawa <k.arkw87@gmail.com>
2022-12-26 22:02:01 +0900

    #14 fix:bugs

--- src/dataaccess/gameRecordGateway.ts
+++ src/dataaccess/gameRecordGateway.ts
@@ -3,11 +3,6 @@ import { addDoc, collection, getDocs, limit, query, orderBy, getCountFromServer 
 import { GameRecord, recordConverter } from "./recordType";
 
 export class GameRecordGateWay {
-    async getNumOfGameRecords(): Promise<number>{
-        const totalCount = await getCountFromServer(query(collection(db, "records")));
-        return totalCount.data().count;
-    }
-
     async postGameRecord(record: GameRecord): Promise<void>{
         addDoc(collection(db, "records").withConverter(recordConverter), {
             id: record.id,
@@ -18,7 +13,7 @@ export class GameRecordGateWay {
         }).catch(()=>alert("ERROR"))
     }
 
-    async getGameRecord(): Promise<GameRecord[]> {
+    async getGameAllRecord(): Promise<GameRecord[]> {
         const snapShot = await getDocs(collection(db, "records").withConverter(recordConverter));
         return snapShot.docs.map((doc) => doc.data());
     }
@@ -26,5 +21,10 @@ export class GameRecordGateWay {
     async getLatestTopFiveGameRecord(): Promise<GameRecord[]> {
         const topFiveScore = await getDocs(query(collection(db, "records").withConverter(recordConverter), orderBy("score", "desc"), limit(5)));
         return topFiveScore.docs.map((doc) => doc.data());
+    }
+
+    async getNumOfGameRecords(): Promise<number>{
+        const totalCount = await getCountFromServer(query(collection(db, "records")));
+        return totalCount.data().count;
     }
 }
