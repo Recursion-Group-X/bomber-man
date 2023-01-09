8f910c4540a47daf5d605a9b8733ebed3854d994
Inaryy <yylovecook@gmail.com>
2023-01-09 17:22:52 +0900

    エネミーの方向がランダムで変わり、方向に基づいての画像が変更されるように変更

--- client/src/models/Enemies.tsx
+++ client/src/models/Enemies.tsx
@@ -1,5 +1,14 @@
 import { Config } from "../bombermanConfig"
-import greenEnemyImg from '../assets/green_enemy.png'
+import enemyFrontImg from '../assets/enemy-front.png'
+import enemyFrontWalk1Img from '../assets/enemy-front-walk1.png'
+import enemyFrontWalk2Img from '../assets/enemy-front-walk2.png'
+import enemyBackImg from '../assets/enemy-back.png'
+import enemyBackWalk1Img from '../assets/enemy-back-walk1.png'
+import enemyBackWalk2Img from '../assets/enemy-back-walk2.png'
+import enemyLeftImg from '../assets/enemy-left.png'
+import enemyLeftWalkImg from '../assets/enemy-left-walk.png'
+import enemyRightImg from '../assets/enemy-right.png'
+import enemyRightWalkImg from '../assets/enemy-right-walk.png'
 
 export class Enemies {
     x: number = 0;
@@ -7,10 +16,9 @@ export class Enemies {
     width: number
     height: number
     direction: string
-
+    pastDirection: string
+    enemyImg: string
     isFirst: boolean =true;
-
-    enemiesList: Object[]
 
     maxBolcks: number
     canvasSize: number = 510
@@ -21,7 +29,6 @@ export class Enemies {
     constructor(config: Config, currentStage: number[][]) {
         this.maxBolcks = config.x;
         if(this.isFirst){
-            console.log(this.isFirst)
             this.inititalDefinePosition(currentStage);
         }else{
             this.definePosition(currentStage);
@@ -30,18 +37,13 @@ export class Enemies {
         this.width = 32;
         this.height = 32;
         this.direction = '';
+        this.pastDirection = 'down';
+        this.enemyImg = enemyFrontImg;
 
-        this.enemiesList = [];
         setInterval(() => {
             this.changeRandomDirection();
         }, 10000);
     }
-
-
-    changeRandomDirection(): void {
-    }
-
-
 
     inititalDefinePosition(currentStage: number[][]): void{
         const max: number = this.maxBolcks;
@@ -56,8 +58,6 @@ export class Enemies {
         this.x = this.boxSize * randomNumX
         this.y = this.boxSize * randomNumY
         this.isFirst = false;
-        console.log(randomNumX)
-        console.log(randomNumY)
     }
 
     definePosition(currentStage: number[][]): void {
@@ -76,7 +76,7 @@ export class Enemies {
 
     drawEnemy(canvas: CanvasRenderingContext2D | null | undefined): void {
         const img = document.createElement('img')
-        img.src = greenEnemyImg
+        img.src = this.changeEnemyImg()
         canvas?.drawImage(
             img,
             this.x,
@@ -89,5 +89,44 @@ export class Enemies {
     moveEnemy(canvas: CanvasRenderingContext2D | null | undefined): void {
         this.drawEnemy(canvas)
     }
+
+    getIndex(position: number): number{
+        return Math.floor(position / this.boxSize)
+    }
+
+    changeRandomDirection(): void {
+        const Directon: string[] = ['up','down','left','right']
+        const randomNum: number = Math.floor(Math.random() * (3 - 0)) + 0;
+
+
+        this.pastDirection = this.direction
+        this.direction = Directon[randomNum]
+    }
+
+    changeEnemyImg(): string{
+        let src: string = enemyFrontImg
+        if(this.direction === ''){
+            if(this.pastDirection === 'up') src = enemyBackImg
+            if(this.pastDirection === 'down') src = enemyFrontImg
+            else if(this.pastDirection === 'left') src = enemyLeftImg
+            else if(this.pastDirection === 'right') src = enemyRightImg
+        } else{
+            if(this.direction === 'down'){
+                src = this.enemyImg === enemyFrontWalk1Img ? enemyFrontWalk2Img : enemyFrontWalk1Img
+            }
+            else if(this.direction === 'up'){
+                src = this.enemyImg === enemyBackWalk1Img ? enemyBackWalk2Img : enemyBackWalk1Img
+            }
+            else if(this.direction === 'left'){
+                src = this.enemyImg === enemyLeftImg ? enemyLeftWalkImg : enemyLeftImg
+            }
+            else if(this.direction === 'right'){
+                src = this.enemyImg === enemyRightImg ? enemyRightWalkImg : enemyRightImg
+            }
+        }
+        this.enemyImg = src
+        return src
+    }
+
 }
 
