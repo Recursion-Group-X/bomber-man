90ac6db30a992e5a490846b5331bc342478b81d0
Takeshi Hashimoto <htakeshi0614@gmail.com>
2023-01-18 06:50:22 -0800

    changed player speed and interval

--- server/player.ts
+++ server/player.ts
@@ -21,7 +21,7 @@ export class Player {
   direction: string = "stay";
   pastDirection: string = "down";
   imageType: number = 1;
-  speed: number = 5;
+  speed: number = 1;
   numOfBombs: number = 1;
   bombPower: number = 1;
   isAlive: boolean = true;
@@ -204,7 +204,7 @@ export class Player {
     } else if (itemType === Stage.stageValues.fireUp) {
       this.bombPower++;
     } else if (itemType === Stage.stageValues.speedUp) {
-      this.speed += 2;
+      this.speed += 0.5;
     }
 
     board[playerIndex.i][playerIndex.j] = Stage.stageValues.ground;
